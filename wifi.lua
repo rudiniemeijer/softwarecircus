a03acfeedfc5cf573d257fed46b425bb61050774
Rudi Niemeijer <rudiniemeijer@gmail.com>
2017-09-06 16:16:28 +0200

    Added a webhook to IFTTT

--- wifi.lua
+++ wifi.lua
@@ -1,9 +1,16 @@
 -- NodeMCU remebers this, even after power-off or reset
 -- so no need to run this more than once
 wifi.setmode(wifi.STATION)
-wifi.sta.config("softwarecircus", "toomanysecrets")
+station_cfg = {}
+station_cfg.ssid = "softwarecircus"
+station_cfg.pwd = "toomanysecrets"
+station_cfg.save = true
+wifi.sta.config(station_cfg)
 wifi.sta.connect()
 
--- Check the status of the IP-configuration
-print(wifi.sta.status()) -- Status 5 is A-OK
-print(wifi.sta.getip())
+-- Check the status of the IP-configuration in the console
+-- print(wifi.sta.status()) -- Status 5 is A-OK
+-- print(wifi.sta.getip())
+
+-- Call a webhook, just for fun
+http.get("http://maker.ifttt.com/trigger/testwifi/with/key/cKOv_7WOkun-XfI22a5Duw-yhCiJF61C-dn4NvuU-LI?value1=" .. wifi.sta.getmac() .. "&value2=" .. wifi.sta.getip())
